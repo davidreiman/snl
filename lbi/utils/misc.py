74357ebfafd2c5ea81c4a8d7dda8ef4c5f05001f
John Tamanas <jtamanas@users.noreply.github.com>
2020-10-12 08:42:31 -0700

    log_dir -> log_path. Added log_path directory prep

--- lbi/utils/misc.py
+++ lbi/utils/misc.py
@@ -42,7 +42,7 @@ def prep_log_path(log_path='./runs/test_run/', settings_path='./settings'):
     path.mkdir(parents=True, exist_ok=True)
     # copy settings folder into new directory
     # TODO: Clean this up?
-    distutils.dir_util.copy_tree(settings_path, str(path.absolute())+'settings/')
+    distutils.dir_util.copy_tree(settings_path, str(path.absolute())+'/settings/')
     # no need for the entire Path (at least on linux). Might need it for windows?
     return str(path.absolute())
 
