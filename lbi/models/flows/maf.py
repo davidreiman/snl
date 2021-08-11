Added conditional moon example. Fixed masking

lbi/models/flows/maf.py
@@ -1,15 +1,13 @@
+from re import A
 import jax.numpy as np
 import flows
 from jax.experimental import stax
 
 
 def get_masks(input_dim, context_dim=0, hidden_dim=64, num_hidden=1):
-    """
-    now adapted to take context
-    """
     masks = []
+    degrees = [np.arange(input_dim)]
     input_degrees = np.arange(input_dim)
-    degrees = [input_degrees]
 
     for n_h in range(num_hidden + 1):
         degrees += [np.arange(hidden_dim) % (input_dim - 1)]
@@ -20,12 +18,9 @@ def get_masks(input_dim, context_dim=0, hidden_dim=64, num_hidden=1):
             np.float32
         )
         if i == 0:
-            mask = np.pad(
-                mask, pad_width=(0, context_dim), constant_values=1
-            )  # to handle context
-        elif i == 1:
             mask = np.vstack((mask, np.ones((context_dim, mask.shape[-1]))))
         masks += [mask]
+
     return masks
 
 
@@ -55,7 +50,11 @@ def MaskedAffineFlow(n_layers=5):
     """
     return flows.Flow(
         transformation=flows.Serial(
-            *(flows.MADE(masked_transform), flows.Reverse()) * n_layers
+            *(
+                flows.MADE(masked_transform),
+                flows.Reverse(),
+            )
+            * n_layers
         ),
         prior=flows.Normal(),
     )
@@ -63,25 +62,85 @@ def MaskedAffineFlow(n_layers=5):
 
 if __name__ == "__main__":
     import jax
+    import optax
+    import torch
+    from torch.utils.data import DataLoader, TensorDataset
     from jax.experimental import stax
+    from sklearn.datasets import make_moons
+    from sklearn.preprocessing import StandardScaler
+    from sklearn.model_selection import train_test_split
+    from tqdm.auto import tqdm
+    import matplotlib.pyplot as plt
 
-    batch = 5
-    input_dim = 3
-    context_dim = 2
-    hidden_dim = 128
-    n_layers = 5
+    def loss(params, inputs, context=None):
+        return -log_pdf(params, inputs, context).mean()
 
-    rng = jax.random.PRNGKey(0)
+    @jax.jit
+    def train_step(params, opt_state, batch):
+        nll, grads = jax.value_and_grad(loss)(params, *batch)
+        updates, opt_state = opt_update(grads, opt_state, params)
+        return nll, optax.apply_updates(params, updates), opt_state
+
+    hidden_dim = 32
+    n_layers = 4
+
+    batch_size = 128
+    seed = 1234
+    nsteps = 400
+
+    X, y = make_moons(n_samples=10000, noise=0.05, random_state=seed)
+    y = y[:, None]
+    input_dim = X.shape[1]
+    context_dim = y.shape[1]
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.05, stratify=y, random_state=seed
+    )
+
+    X_train_s = torch.tensor(X_train, dtype=torch.float32)
+    y_train = torch.tensor(y_train, dtype=torch.float32)
+
+    train_dataloader = DataLoader(
+        TensorDataset(X_train_s, y_train),
+        batch_size=batch_size,
+        shuffle=True
+        # TensorDataset(X_train_s), batch_size=batch_size, shuffle=True
+    )
+
+    rng = jax.random.PRNGKey(seed)
     params, log_pdf, sample = MaskedAffineFlow(n_layers=n_layers)(
         rng,
         input_dim=input_dim,
         context_dim=context_dim,
+        # context_dim=0,
         hidden_dim=hidden_dim,
     )
-    # print(params)
-    print(
-        log_pdf(
-            params, np.zeros((batch, input_dim)), context=np.zeros((batch, context_dim))
-        )
+
+    learning_rate = 1e-4
+    opt_init, opt_update = optax.chain(
+        # Set the parameters of Adam. Note the learning_rate is not here.
+        optax.adamw(learning_rate=learning_rate),
     )
-    # print(log_pdf(params, np.ones((batch, input_dim))))
+
+    opt_state = opt_init(params)
+
+    iterator = tqdm(range(nsteps))
+    try:
+        for _ in iterator:
+            for batch in train_dataloader:
+                batch = [np.array(a) for a in batch]
+                nll, params, opt_state = train_step(params, opt_state, batch)
+            iterator.set_description("nll = {:.3f}".format(nll))
+    except KeyboardInterrupt:
+        pass
+
+    plt.scatter(*X_train.T, color="grey", alpha=0.01, marker=".")
+
+    samples_0 = sample(rng, params, context=np.zeros((1000, context_dim)))
+    plt.scatter(*samples_0.T, color="red", label="0", marker=".", alpha=0.2)
+    samples_1 = sample(rng, params, context=np.ones((1000, context_dim)))
+    plt.scatter(*samples_1.T, color="blue", label="1", marker=".", alpha=0.2)
+
+    plt.xlim(-1.5, 2.5)
+    plt.ylim(-1, 1.5)
+    plt.legend()
+    plt.show()